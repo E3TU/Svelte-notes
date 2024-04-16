1f426cb9b2574fd1eaf2b18cd81d2f4b9a70dd13
Eetu <99178022+E3TU@users.noreply.github.com>
2024-04-16 23:43:59 +0300

    Added sidebar component

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,25 @@
+<div class="sidebar-container">
+    <h2 class="navbar-heading">Svelte Notes</h2>
+    <button id="newnote-btn">New Note</button>
+</div>
+
+<style lang="scss">
+    .sidebar-container{
+        height: 100vh;
+        width: 20rem;
+        background-color: $primary-color;
+        margin: 1rem;
+        border-radius: 12px;
+    }
+    .navbar-heading{
+        margin: 1rem;
+        color: $white;
+        letter-spacing: 0.1rem;
+    }
+    #newnote-btn{
+        margin: 1rem;
+        background-color: $black;
+        border: none;
+        color: $white;
+    }
+</style>
