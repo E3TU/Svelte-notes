3617da6a150ac9c61a8a65e4fb0096e793b70401
Eetu <99178022+E3TU@users.noreply.github.com>
2024-04-16 23:43:15 +0300

    Install font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,22 @@
+<script>
+    // Import Font
+    import '@fontsource-variable/rubik';
+
+    // Import Components
+    import Sidebar from "../components/Sidebar.svelte";
+</script>
+
 <div class="flex-container">
-    <h1>Test</h1>
+    <Sidebar/>
 </div>
 
 <style lang="scss">
     .flex-container{
         display: flex;
+        flex-direction: column;
         height: 100vh;
         width: 100vw;
-        background-color: #000;
+        background-color: $bg-color;
+        font-family: 'Rubik Variable', sans-serif;
     }
 </style>
