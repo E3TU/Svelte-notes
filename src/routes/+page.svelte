ec4ae860d7f9ebbf1057daad90aee65508abebb8
Eetu <99178022+E3TU@users.noreply.github.com>
2024-04-16 22:52:31 +0300

    Initialized project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="flex-container">
+    <h1>Test</h1>
+</div>
+
+<style lang="scss">
+    .flex-container{
+        display: flex;
+        height: 100vh;
+        width: 100vw;
+        background-color: #000;
+    }
+</style>
