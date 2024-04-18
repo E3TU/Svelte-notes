93bf7f4f56dd75ef456ecdf9709bb9d6c32504eb
Eetu <99178022+E3TU@users.noreply.github.com>
2024-04-18 11:16:03 +0300

    Updated flexbox + added notes component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,39 @@
 <script>
-    // Import Font
-    import '@fontsource-variable/rubik';
+  // Import Font
+  import "@fontsource-variable/rubik";
 
-    // Import Components
-    import Sidebar from "../components/Sidebar.svelte";
-    import Searchbar from '../components/Searchbar.svelte';
+  // Import Components
+  import Sidebar from "../components/Sidebar.svelte";
+  import Searchbar from "../components/Searchbar.svelte";
+  import Notes from "../components/Notes.svelte";
 </script>
 
 <div class="flex-container">
-    <Searchbar/>
-    <Sidebar/>
+    <div class="flex-items-left">
+        <Sidebar />
+    </div>
+  <div class="flex-items-right">
+    <Searchbar />
+    <Notes />
+  </div>
 </div>
 
 <style lang="scss">
-    .flex-container{
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-        width: 100vw;
-        background-color: $bg-color;
-        font-family: 'Rubik Variable', sans-serif;
-    }
+  .flex-container {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    width: 100vw;
+    background-color: $bg-color;
+    font-family: "Rubik Variable", sans-serif;
+  }
+  .flex-items-left{
+    display: flex;
+    flex-direction: column;
+  }
+  .flex-items-right{
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
 </style>
