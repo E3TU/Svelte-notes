c755011d7d5acbfaf907b5316a0d3c9c5c20d426
Eetu <99178022+E3TU@users.noreply.github.com>
2024-04-18 11:16:16 +0300

    Created Notes component

---
+++ src/components/Notes.svelte
@@ -0,0 +1,13 @@
+<div class="notes-container">
+
+</div>
+
+<style lang="scss">
+    .notes-container{
+        margin: 1rem;
+        background-color: $black;
+        height: 100%;
+        border-radius: 12px;
+        width: auto;
+    }
+</style>
