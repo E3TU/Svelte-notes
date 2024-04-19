dbf0bbd3f246958bb4584058d26cf46932eafd8f
Eetu <99178022+E3TU@users.noreply.github.com>
2024-04-20 01:23:52 +0300

    Added search button + icon

--- src/components/Searchbar.svelte
+++ src/components/Searchbar.svelte
@@ -1,14 +1,21 @@
+<script>
+    import Icon from '@iconify/svelte';
+</script>
+
 <div class="searchbar">
     <input type="text" placeholder="Search For Note...">
+    <button id="search-btn"><Icon icon="grommet-icons:search" color="#fff" /></button>
 </div>
 
 <style lang="scss">
     .searchbar{
-        width: auto;
+        background-color: transparent;
+        display: flex;
+        flex-direction: row;
+        width: 98%;
         height: 4rem;
         margin: 1rem;
         border-radius: 12px;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     }
     input{
         background-color: $gray;
@@ -19,9 +26,23 @@
         color: $white;
         padding: 1rem;
         font-size: 1.25rem;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     }
     ::placeholder{
         color: $white;
         opacity: 0.8;
+    }
+    #search-btn{
+        margin-left: 2rem;
+        padding: 1rem 1.5rem 1rem 1.5rem;
+        border-radius: 12px;
+        border: none;
+        background-color: $primary-color;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+        transition: 0.5s;
+    }
+    #search-btn:hover{
+        opacity: 0.8;
+        transition: 0.5s;
     }
 </style>
