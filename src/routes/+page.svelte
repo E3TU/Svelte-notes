4491577f4fbf4f909acccfd9a57a33d0c6279a2b
Eetu <99178022+E3TU@users.noreply.github.com>
2024-10-20 14:56:05 +0300

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,8 @@
   let isOpen = false;
 
   // Function to open/close note creation menu
-  function createNoteMenu() {
+  function createNoteMenu(e) {
+      e.stopPropagation();
       isOpen = !isOpen;
   }
   
@@ -42,11 +43,21 @@
             <div class="createnote">
                 <div class="top-section">
                     <h1 class="createnote-heading">Create new note</h1>
-                    <button class="closemenu-btn" on:click={createNoteMenu}>
+                    <button class="closemenu-btn" on:click|preventDefault={createNoteMenu}>
                         <Icon class="closemenu-icon" icon="basil:cross-outline"/>
                     </button>
                 </div>
-                <input>
+                <div class="bottom-section">
+                    <div class="note-title-wrapper">
+                        <label for="note-title">Note Title</label>
+                        <input id="note-title" placeholder="Enter note title">
+                    </div>
+                    <div class="note-content-wrapper">
+                        <label for="note-content">Note Content</label>
+                        <textarea id="note-content" placeholder="Write your note here..." rows="10"></textarea>
+                    </div>
+                    <button id="save-note-btn">Save Note</button>
+                </div>
             </div>
         </div>
     {/if}
@@ -67,28 +78,29 @@
     width: 100vw;
     background-color: $bg-color;
     font-family: "Rubik Variable", sans-serif;
+    z-index: 1000;
   }
   .createnote-container{
     display: flex;
     justify-content: center;
     align-items: center;
-    transition: opacity 0.3s ease; /* Smooth transition for opacity */
     position: fixed;
     width: 100vw;
     height: 100vh;
-    z-index: 1000;
+    z-index: 999;
     backdrop-filter: blur(1px); /* Blurs the background */
   }
   .createnote{
     display: flex;
     flex-direction: column;
     margin-top: 2rem;
-    width: 35rem;
-    height: 45rem;
+    width: 30rem;
+    height: 40rem;
     background-color: $bg-color;
     border-radius: 12px;
     box-shadow: rgba(0, 0, 0, 0.10) 0px 2px 4px 0px, rgba(0, 0, 0, 0.80) 0px 2px 16px 0px;
     color: $white;
+    cursor: default !important;
   }
   .top-section{
     display: flex;
@@ -101,7 +113,7 @@
   }
   .closemenu-btn{
     height: 2rem;
-    width: 4rem;
+    width: 2rem;
     margin-left: auto;
     margin-top: 1rem;
     background: transparent;
@@ -113,6 +125,83 @@
   :global(.closemenu-icon) {
     font-size: 2rem;
     color: $white;
+  }
+  .bottom-section{
+    display: flex;
+    flex-direction: column;
+  }
+  .note-title-wrapper{
+    display: flex;
+    flex-direction: column;
+  }
+  .note-content-wrapper{
+    display: flex;
+    flex-direction: column;
+  }
+  label{
+    margin-top: 1rem;
+    margin-left: 1rem;
+    font-size: 1.25rem;
+  }
+  #note-title{
+    padding: 1rem;
+    margin: 1rem;
+    font-size: 1rem;
+    border-radius: 8px;
+    outline: none;
+    border: none;
+  }
+  #note-content{
+    resize: none;
+    margin: 1rem;
+    padding: 1rem;
+    font-size: 1rem;
+    border-radius: 8px;
+    border: none;
+    outline: none;
+  }
+  #save-note-btn{
+    position: relative;
+    padding: 1rem;
+    margin: 1rem;
+    font-size: 1.25rem;
+    border-radius: 8px;
+    outline: none;
+    border: 2px solid $primary-color;
+    background-color: $primary-color;
+    color: $white;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    box-sizing: border-box;
+    display: inline-block;
+    overflow: hidden;
+    white-space: nowrap;
+    z-index: 0;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+  #save-note-btn::before{
+    position: absolute;
+    content: "";
+    background: $primary-color;
+    transition-duration: 0.5s;
+    z-index: -1;
+    inset: 0px 0px auto auto;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+  }
+  #save-note-btn:hover::before{
+    width: 0px;
+    height: 100%;
+    opacity: 1;
+  }
+  #save-note-btn:hover{
+    background-color: transparent;
+    transition: 0.5s;
+    color: $primary-color;
+  }
+  #save-note-btn:active{
+    top: 2px;
   }
   .flex-items-left{
     display: flex;
