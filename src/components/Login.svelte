2876bac9c999b202f196765054605371a289f8d9
Eetu <99178022+E3TU@users.noreply.github.com>
2024-10-27 16:35:48 +0200

    Login component

---
+++ src/components/Login.svelte
@@ -0,0 +1,6 @@
+<div class="login-container">
+    <form class="login-form">
+        <input class="login-input" type="text" placeholder="Email" />
+        <input type="password" class="login-input" placeholder="Password" />
+    </form>
+</div>
