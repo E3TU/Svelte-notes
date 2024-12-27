b4ae6d4873c99b7de999c8d3dbd977ee9d9d75db
Eetu <99178022+E3TU@users.noreply.github.com>
2024-12-27 20:33:46 +0200

    Upgraded to dotnet9.0

--- aspnet/NotesBackend/Program.cs
+++ aspnet/NotesBackend/Program.cs
@@ -1,9 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 builder.Services.AddCors();
 
 var app = builder.Build();
@@ -11,8 +10,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
