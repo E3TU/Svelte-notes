a40c72266fc82715b5dda6c939b3e2ba55f24183
Eetu <99178022+E3TU@users.noreply.github.com>
2025-01-10 21:20:14 +0200

    Formatted code

--- sveltekit/src/stores/userStore.js
+++ sveltekit/src/stores/userStore.js
@@ -1,49 +1,48 @@
 import { writable } from "svelte/store";
-import { account, ID } from '$lib/appwrite';
+import { account, ID } from "$lib/appwrite";
 import { goto } from "$app/navigation";
 
-const isBrowser = typeof window !== 'undefined';
+const isBrowser = typeof window !== "undefined";
 
 const createUser = () => {
-    const store = writable(null);
+  const store = writable(null);
 
-    async function init() {
-        if (!isBrowser) return;
-        try{
-            store.set(await account.get());
-        } catch (e) {
-            store.set(null);
-        }
+  async function init() {
+    if (!isBrowser) return;
+    try {
+      store.set(await account.get());
+    } catch (e) {
+      store.set(null);
     }
+  }
 
+  init();
 
-    init();
+  async function register(email, password, username) {
+    await account.create(ID.unique(), email, password, username);
 
-    async function register(email, password, username) {
-        await account.create(ID.unique(), email, password, username);
+    login(email, password);
+  }
 
-        login(email, password);
-    }
+  async function login(email, password) {
+    await account.createEmailPasswordSession(email, password);
+    // loggedInUser = await account.get();
+    await init();
+    goto("/");
+  }
 
-    async function login(email, password) {
-        await account.createEmailPasswordSession(email, password);
-        // loggedInUser = await account.get();
-        await init();
-        goto('/');
-    }
+  async function logout() {
+    await account.deleteSession("current");
+    store.set(null);
+  }
 
-    async function logout() {
-        await account.deleteSession('current');
-        store.set(null);
-    }
-
-    return{
-        subscribe: store.subscribe,
-        register,
-        login,
-        logout,
-        init
-    };
+  return {
+    subscribe: store.subscribe,
+    register,
+    login,
+    logout,
+    init,
+  };
 };
 
 export const user = createUser();
