cbf881786e7611fdeee668cba030a563094dfd98
Eetu <99178022+E3TU@users.noreply.github.com>
2025-01-10 23:50:52 +0200

    Added variable for endpoint

--- sveltekit/src/lib/appwrite.js
+++ sveltekit/src/lib/appwrite.js
@@ -1,11 +1,15 @@
 import { Client, Account } from "appwrite";
 
+// Appwrite endpoint
+const endpoint = "http://localhost:/v1";
+
+// Appwrite project id
 const projectId = import.meta.env.VITE_PROJECT_ID;
 
 export const client = new Client();
 
 client
-    .setEndpoint("http://localhost:/v1")
+    .setEndpoint(endpoint)
     .setProject(projectId);
 
 export const account = new Account(client);
