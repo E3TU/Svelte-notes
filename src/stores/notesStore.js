4af57e65d010d1c6f3d1920bc1bf4465a0144d34
Eetu <99178022+E3TU@users.noreply.github.com>
2025-01-30 19:49:55 +0200

    Added update note function

--- src/stores/notesStore.js
+++ src/stores/notesStore.js
@@ -19,6 +19,18 @@ export function addNote(noteTitle, noteContent) {
   });
 }
 
+export function editNote(id, newTitle, newContent) {
+  notes.update((currentNotes) => {
+    return currentNotes.map((note) => {
+      if (note.id === id) {
+        return { ...note, title: newTitle, content: newContent };
+      }
+
+      return note;
+    });
+  });
+}
+
 export function deleteNote(id) {
   //   notes = notes.filter((note) => note.id !== id);
     notes.update((currentNotes) => currentNotes.filter((note) => note.id !== id));
