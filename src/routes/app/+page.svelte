2ee01e51ff7354d3865420782249bf54b510befc
Eetu <99178022+E3TU@users.noreply.github.com>
2025-01-30 21:19:24 +0200

    Renamed component

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -3,7 +3,7 @@
     import Notes from "../../components/Notes.svelte";
     import Searchbar from "../../components/Searchbar.svelte";
     import Sidebar from "../../components/Sidebar.svelte";
-    import Createnote from "../../components/Createnote.svelte";
+    import Createnotemodal from "../../components/Createnotemodal.svelte";
 
     // Import Store(s)
     import {createNoteMenu, isMenuOpen} from "../../stores/CreateNoteMenu";
@@ -12,7 +12,7 @@
 
 <div class="flex-container">
     {#if $isMenuOpen}
-       <Createnote />
+       <Createnotemodal />
    {/if}
    <div class="flex-items-left">
        <Sidebar />
