2182d9c0bbd89e368da4e9b073fc70c7efced47f
Eetu <99178022+E3TU@users.noreply.github.com>
2025-02-09 23:23:47 +0200

    Created route for api

---
+++ src/routes/api/notes.js
@@ -0,0 +1,9 @@
+import { DATABASE_ID, COLLECTION_ID } from "$env/static/public";
+
+
+import { createSessionClient } from "$lib/appwrite";
+import { Databases } from "appwrite";
+
+const databaseId = DATABASE_ID;
+const collectionId = COLLECTION_ID;
+
