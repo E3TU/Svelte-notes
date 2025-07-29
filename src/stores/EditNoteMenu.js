734af9b28838cdcb45a0606c503c8bcc35388008
Eetu <99178022+E3TU@users.noreply.github.com>
2025-07-29 09:24:28 +0300

    added selectedNoteid

--- src/stores/EditNoteMenu.js
+++ src/stores/EditNoteMenu.js
@@ -1,11 +1,17 @@
 //Store to open and close note editing modal
 
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 // Note editing menu is closed as default
 export const isEditNoteMenuOpen = writable(false);
+export const selectedNoteId = writable(null);
 
 // Function to open the note editing menu
-export function editNoteMenu() {
-    isEditNoteMenuOpen.update(value => !value);
+export function editNoteMenu(id) {
+  selectedNoteId.set(id);
+  isEditNoteMenuOpen.update((value) => !value);
+
+//   selectedNoteId.subscribe((value) => {
+//     console.log(value);
+//   });
 }
