a35e028797d8be088df715620e0a11a4aa98cef5
Eetu <99178022+E3TU@users.noreply.github.com>
2025-08-14 09:34:24 +0300

    Created store for categories

---
+++ src/stores/categoriesStore.js
@@ -0,0 +1,15 @@
+import { writable } from "svelte/store";
+
+export let categories = writable([]);
+export let categoryName = writable("");
+
+export async function fetchCategories() {
+  const res = await fetch("api/categories");
+  const data = await res.json();
+
+  categories.set(data.collections);
+
+  categories.subscribe((value) => {
+    console.log(value);
+  });
+}
