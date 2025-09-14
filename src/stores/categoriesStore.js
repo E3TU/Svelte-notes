b570f4458a982e05a8cdd370f5c27ba852d2e696
Eetu <99178022+E3TU@users.noreply.github.com>
2025-09-15 00:09:56 +0300

    Derive first category from categories store

--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -1,47 +1,24 @@
-import { writable } from "svelte/store";
+import { writable, derived } from "svelte/store";
 
 export let categories = writable([]);
-export let categoryNames = writable([]);
+export let firstCategory = derived([categories], ([$categories]) => $categories[0]); 
 
-export let firstCategory = writable(null);
+// export let firstCategory = writable(null);
 
-export async function fetchCategoryNames() {
+export async function fetchCategories() {
   const res = await fetch("api/categories");
   const data = await res.json();
 
+  // Set collection data to categories store
   categories.set(data.collections);
 
-  for (let i = 0; i < data.collections.length; i++) {
-    // console.log(data.collections[i].name);
-    categoryNames.update((names) => {
-      names.push(data.collections[i].name);
-      return names;
-    });
-  }
+  // console.log(data);
 
-
-  // categories.subscribe((value) => {
-  //   console.log(value[0]);
-  // });
-
-  // firstCategory.subscribe((value) => {
-  //   console.log(value);
-  // });
-
-  // categoryNames.subscribe((value) => {
-  //   console.log(value);
-  // });
-
-  // categories.subscribe((value) => {
-  //   console.log(value);
-  // });
+  fetchFirstCategory();
 }
 
 export async function fetchFirstCategory() {
-  const res = await fetch("api/categories");
-  const data = await res.json();
-
-  categories.subscribe((value) => {
+  firstCategory.subscribe((value) => {
     console.log(value);
   });
 }
