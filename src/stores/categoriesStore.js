595683eab6f86efa1b00bee520b2f34a94fec6e4
Eetu <99178022+E3TU@users.noreply.github.com>
2025-09-21 00:02:46 +0300

    Remove  unused shit

--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -6,14 +6,7 @@ export const firstCategoryId = derived(
   [categories],
   ([$categories]) => $categories[0].$id
 );
-// export const collectionId = derived([firstCategory], ([$firstCategory]) => $firstCategory.$id);
 
-// export const categoryId = derived([categories], ([$categories]) => $categories[0]?.$id);
-
-export const categoryId = writable();
-// export const selectedCategoryId = writable();
-
-// export let firstCategory = writable(null);
 
 export async function fetchCategories() {
   const res = await fetch("api/categories");
@@ -30,8 +23,7 @@ export async function fetchCategories() {
 export async function fetchFirstCategory() {
   firstCategoryId.subscribe(async (value) => {
     // console.log(value);
-    // categoryId.set(value);
-
+    
     const res = await fetch("api/updateid", {
       method: "POST",
       headers: {
@@ -45,8 +37,6 @@ export async function fetchFirstCategory() {
 }
 
 export async function updateCategory($id) {
-  // categoryId.set($id);
-  
   const res = await fetch("/api/updateid", {
     method: "POST",
     headers: {
