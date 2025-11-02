20aad7af1fd4c0e4356f3ee9d1966af241f10b9e
Eetu <99178022+E3TU@users.noreply.github.com>
2025-11-02 23:10:36 +0200

    Post function

--- src/stores/account.js
+++ src/stores/account.js
@@ -10,7 +10,15 @@ export async function fetchUserName() {
 }
 
 export async function updateUsername() {
-    
+  userName.subscribe(async (value) => {
+    const res = await fetch("/api/account", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        action: "updateusername",
+        username: value,
+      }),
+    });
+    const data = await res.json();
+  });
 }
-
-
