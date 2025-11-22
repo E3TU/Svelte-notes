9cf26fbb24a3da9a6495c1d6e30aa1933fe18dfc
Eetu <99178022+E3TU@users.noreply.github.com>
2025-11-22 16:15:04 +0200

    List only user specific documents

--- src/routes/api/categories/+server.js
+++ src/routes/api/categories/+server.js
@@ -1,24 +1,32 @@
 import { json } from "@sveltejs/kit";
-import { createAdminClient, createSessionClient } from "../../../lib/server/appwrite.js";
-import { ID, Databases, Role, Permission } from "node-appwrite";
+import {
+  createAdminClient,
+  createSessionClient,
+} from "../../../lib/server/appwrite.js";
+import { ID, Databases, Role, Permission, Query } from "node-appwrite";
 
 const databaseId = import.meta.env.VITE_DATABASE_ID;
 const categoriesCollectionId = import.meta.env.VITE_CATEGORIES_COLLECTION_ID;
 
-export async function GET({ locals }) {
-  const { account } = createAdminClient(locals);
+export async function GET(event) {
+  const { account } = createSessionClient(event);
   const databases = new Databases(account.client);
+
+  const user = await account.get();
+  const userId = user.$id;
 
   try {
     const fetchCategoriesResponse = await databases.listDocuments(
       databaseId,
-      categoriesCollectionId
+      categoriesCollectionId,
+      [
+        Query.equal("userId", userId)
+      ]
     );
 
     let categories = fetchCategoriesResponse.documents;
 
     return json({ documents: categories });
-
   } catch (error) {
     console.error(error);
     return json({ error: "Failed to fetch categories" }, { status: 500 });
@@ -29,7 +37,7 @@ export async function POST(event) {
   const { request } = event;
 
   const { account } = createSessionClient(event);
-  
+
   const databases = new Databases(account.client);
 
   try {
@@ -43,10 +51,7 @@ export async function POST(event) {
       categoriesCollectionId,
       ID.unique(),
       { title: categoryName, userId: userId },
-      [
-        Permission.read(Role.user(userId)), 
-        Permission.write(Role.user(userId)),
-      ]
+      [Permission.read(Role.user(userId)), Permission.write(Role.user(userId))]
     );
 
     return json(createCategory);
